4203a4172bad2cdd565f99adecf9510621214ce6
rafaroeder <116201758+rafaroeder@users.noreply.github.com>
2024-02-02 17:03:50 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,9 @@ function setup() {
   }
   
   function draw() {
+    background(30,100,255,100);
     fill(255, 255, 0);
     circle(50, 50, 50);
     rect(windowWidth/2,windowHeight/2,10);
+    rect(20,20,10);
   }
